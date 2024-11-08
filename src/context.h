5694177d7f7fa4013b1dc5281f15c9e3a094a7eb
surkim <ksro0128@naver.com>
2024-11-08 17:14:36 +0900

    13-03
    지연 렌더링 적용

--- src/context.h
+++ src/context.h
@@ -98,6 +98,17 @@ private:
     TextureUPtr m_brickDiffuseTexture;
     TextureUPtr m_brickNormalTexture;
     ProgramUPtr m_normalProgram;
+
+    // deferred shading
+    FramebufferUPtr m_deferGeoFramebuffer;
+    ProgramUPtr m_deferGeoProgram;
+
+    ProgramUPtr m_deferLightProgram;
+    struct DeferLight {
+        glm::vec3 position;
+        glm::vec3 color;
+    };
+    std::vector<DeferLight> m_deferLights;
 };
 
 #endif // __CONTEXT_H__
